1906275a7d4cfaaed3e65779fd4de46b36b32ebe
Bernard <bernarddira@gmail.com>
2025-07-07 20:05:38 +0200

    Add the transaction router

---
+++ Backend/src/routes/transactionRoutes.js
@@ -0,0 +1,16 @@
+import {
+  createTransaction,
+  getTransaction,
+  updateTransaction,
+  deleteTransaction,
+} from "../controllers/transactionController";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", createTransaction);
+router.get("/", getTransaction);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
+
+export default router;
