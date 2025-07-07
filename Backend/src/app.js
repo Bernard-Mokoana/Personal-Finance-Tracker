43acbac48e78a4908e0c1bba0515873c0d47d23e
Bernard <bernarddira@gmail.com>
2025-07-07 20:07:10 +0200

    update the app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,5 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 import authRoute from "./routes/authRoutes.js";
+import authTransaction from "./routes/transactionRoutes.js";
 
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/transaction", authTransaction);
