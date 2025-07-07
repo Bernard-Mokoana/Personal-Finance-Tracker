7e7461931a00ba04b3620095492590a794e2f005
Bernard <bernarddira@gmail.com>
2025-07-07 19:19:20 +0200

    Update the Transaction model

--- Backend/src/model/Transaction.js
+++ Backend/src/model/Transaction.js
@@ -1,26 +1,37 @@
 import mongoose, { Schema } from "mongoose";
 
-const TransactionSchema = new Schema({
-  User: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
+const TransactionSchema = new Schema(
+  {
+    User: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: Array[String],
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  category: {
-    type: Array[String],
-    required: true,
-    trim: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  date: Date,
-  Timestamp,
-});
+  { timestamps: true }
+);
 
 export const transaction = mongoose.model("transaction", TransactionSchema);
