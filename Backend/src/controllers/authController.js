a18b7f5bcff1c097a33694d6b6ad21de65869905
Bernard <bernarddira@gmail.com>
2025-07-08 12:16:12 +0200

    "Update the login method with generate tokens"

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,6 +1,16 @@
 import { users } from "../model/user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 
 export const register = async (req, res) => {
   const { fullname, username, email, password } = req.body;
@@ -33,4 +43,39 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    const user = await users.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign({ userId: user._id }, ACCESS_TOKEN_SECRET, {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    });
+
+    const refreshToken = jwt.sign({ userId: user._id }, REFRESH_TOKEN_SECRET, {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    });
+
+    return res.status(200).json({
+      message: "User login successfully",
+      token,
+      refreshToken,
+      user: { id: user._id, email: user.email },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error logging a user", error: error.message });
+  }
+};
